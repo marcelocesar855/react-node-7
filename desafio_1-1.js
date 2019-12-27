8e5b94c58fa53cd72c58f78e1aaaa4ab3b1a965c
Marcelo Cesar Pereira Monteiro <marcelo.monteiro@sinesp.df.gov.br>
2019-12-27 17:06:52 -0300

    m

---
+++ desafio_1-1.js
@@ -0,0 +1,10 @@
+function exerc1() {
+    var endereco = {
+        rua: "Rua dos pinheiros",
+        numero: 1293,
+        bairro: "Centro",
+        cidade: "São Paulo",
+        uf: "SP"
+       };
+    return `O usuário mora em ${endereco.cidade} / ${endereco.uf}, no bairro ${endereco.bairro}, na rua "${endereco.rua}" com nº ${endereco.numero}`
+}
