e0ca8398fef4b2c0710e90a0b6115f181bd9be83
Marcelo Cesar Pereira Monteiro <marcelo.monteiro@SINESP.DF.GOV.BR>
2019-12-27 17:46:28 -0300

    m

--- desafio_1-1.js
+++ desafio_1-1.js
@@ -8,3 +8,23 @@ function exerc1() {
        };
     return `O usuário mora em ${endereco.cidade} / ${endereco.uf}, no bairro ${endereco.bairro}, na rua "${endereco.rua}" com nº ${endereco.numero}`
 }
+
+function exerc2(x, y) {
+    var qnt = y - x
+    var pares = [qnt]
+    for (i = 0; i <= qnt; i++){
+        if (((x + i) % 2) == 0){
+            pares[i] = x + i
+        }
+    }
+    return pares
+}
+
+function exerc3(skills) {
+    for (i = 0; i < skills.length(); i++) {
+        if (skills[i] == 'Javascript') {
+            return true
+        }
+    }
+    return false
+}
