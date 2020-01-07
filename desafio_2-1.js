8956741e4448e777e2f9c4d5c18e13ec544ac41e
marcelo cesar <marcelocesar855@gmail.com>
2020-01-06 22:03:43 -0300

    -m

--- desafio_2-1.js
+++ desafio_2-1.js
@@ -93,4 +93,36 @@ function mostraInfo({nome, idade}) {
 }
 
 console.log(mostraInfo({ nome: 'Diego', idade: 23 }))
-   
+
+const arr1 = [1, 2, 3, 4, 5, 6]
+
+const [x, ...y] = arr1
+
+console.log(x); 
+console.log(y);
+
+const soma = (...params) => {
+   return params.reduce((total, next) => total + next)
+}
+
+console.log(soma(1,2,3,4,5,6))
+
+const usuario1 = {
+    nome: 'Diego',
+    idade: 23,
+    endereco: {
+    cidade: 'Rio do Sul',
+    uf: 'SC',
+    pais: 'Brasil',
+    }
+};
+
+const usuario2 = {...usuario1, nome : 'Gabriel'}
+
+const usuario3 = {...usuario1, endereco : {
+        ...usuario1.endereco, cidade : 'Lontras'
+    }}
+
+console.log(usuario2)
+
+console.log(usuario3)
